7a29bd319202064ec4b22fc0a30cd036b2bdaecb
Israel Fermín Montilla <israel@ringtu.com>
2014-02-26 11:13:24 -0430

    Adelantos en spider de presidenciales_2006. Llega hasta municipios

--- cne/spiders/presidenciales_2006.py
+++ cne/spiders/presidenciales_2006.py
@@ -5,21 +5,54 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from cne.items import Common, InformacionMesa, ResultadoMesa
 from scrapy.http.request import Request
 
-class Presidenciales2013y2012Spider(BaseSpider):
-    name = 'presidenciales_2013'
+class Presidenciales2006(BaseSpider):
+    name = 'presidenciales_2006'
     start_urls = [
-            'http://www.cne.gob.ve/divulgacionPresidencial/resultado_nacional.php'
+        'http://www.cne.gob.ve/divulgacionPresidencial/resultado_nacional.php'
     ]
 
     def parse(self, response):
-        yield Request(url, meta={'common_info': comun}, callback=self.parse_estado)
+        selector = Selector(response)
+        tables_width_300 = selector.xpath('//table[@width="300"]') # Los estados están en una tabla de estas características
+        odd_rows = tables_width_300[1].xpath('.//tr[@bgcolor="#EEEEEE"]')
+        even_rows = tables_width_300[1].xpath('.//tr[@bgcolor="#DDDDDD"]')
+        
+        states = odd_rows.xpath('.//a') + even_rows.xpath('.//a')
+        states += tables_width_300[2].xpath('.//tr[@bgcolor="#EEEEEE"]').xpath('.//a')
+
+        for state in states:
+            name = state.xpath('.//text()')[0].extract()
+            comun = Common(proceso='Presidenciales', anio=2013, estado=name.strip())
+
+            location = state.xpath('.//@href')[0].extract()
+            url_tokens = response.url.split('/')
+            url_tokens[-1] = location
+            url = '/'.join(url_tokens)
+
+            yield Request(url, meta={'common_info': comun}, callback=self.parse_estado)
 
     def parse_estado(self, response):
-        yield Request(url, meta={'common_info': comun}, callback=self.parse_municipio)
+        comun = response.meta['common_info']
+        selector = Selector(response)
+        tables_width_300 = selector.xpath('//table[@width="300"]')
+        odd_rows = tables_width_300.xpath('.//tr[@bgcolor="#EEEEEE"]')
+        even_rows = tables_width_300.xpath('.//tr[@bgcolor="#DDDDDD"]')
+        municipios = odd_rows.xpath('.//a') + even_rows.xpath('.//a')
+
+        for municipio in municipios:
+            name = municipio.xpath('.//text()')[0].extract()
+            comun['municipio'] = name.strip()
+            location = municipio.xpath('.//@href')[0].extract()
+            url_tokens = response.url.split('/')
+            url_tokens[-1] = location
+            url = '/'.join(url_tokens)
+
+            yield Request(url, meta={'common_info': comun}, callback=self.parse_municipio)
 
 
     def parse_municipio(self, response):
-        yield Request(url, meta={'common_info': comun}, callback=self.parse_parroquia)
+        return Common()
+        #yield Request(url, meta={'common_info': comun}, callback=self.parse_parroquia)
 
     def parse_parroquia(self, response):
         yield Request(url, meta={'common_info': comun}, callback=self.parse_centro)
