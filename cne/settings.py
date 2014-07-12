4a542a5e3f455c7e1982e918715c005e986d9d7b
Israel Fermin Montilla <israel.montilla@dubizzle.com>
2014-07-12 16:40:00 +0400

    Agrega la configuracion de la base de datos MySQL, debe ser cambiado por postgres

--- cne/settings.py
+++ cne/settings.py
@@ -5,6 +5,8 @@
 #
 #     http://doc.scrapy.org/en/latest/topics/settings.html
 #
+import os
+
 
 BOT_NAME = 'cne'
 
@@ -15,8 +17,27 @@ CONCURRENT_ITEMS = 200
 CONCURRENT_REQUESTS = 50
 DOWNLOAD_DELAY = 0.01
 
-#USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:26.0) Gecko/20100101 Firefox/26.0'
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:26.0) Gecko/20100101 Firefox/26.0'
 
 ITEM_PIPELINES = {
     'cne.pipelines.FormatDataPipeline': 200,
 }
+
+INSTALL_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+AGGREGATE_DB_CONF = { 
+    'db': os.environ.get('CNE_DB_NAME', 'scrapy'),
+    'user': os.environ.get('CNE_DB_USER', 'scrapy'),
+    'password': os.environ.get('CNE_DB_PASSWORD', 'scrapy'),
+    'host': os.environ.get('CNE_DB_HOST', 'localhost'),
+    'port': os.environ.get('CNE_DB_PORT', 3306)
+}
+
+AGGREGATE_DB = create_engine(
+    'mysql+mysqldb://%(user)s:%(password)s@%(host)s:%(port)s/%(db)s' % AGGREGATE_DB_CONF, 
+    echo=False,
+    encoding='utf-8',
+    convert_unicode=True,
+)
+
+AGGREGATION_ENABLED = True
